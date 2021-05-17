b78cb85bb0bd70222984a7fe286d30d37563c1ac
jundalisay <jundalisay@yahoo.com>
2021-05-17 10:57:24 +0800

    update

--- config/prod.exs
+++ config/prod.exs
@@ -11,7 +11,8 @@ use Mix.Config
 # before starting your production server.
 config :pantry, PantryWeb.Endpoint,
   http: [port: {:system, "PORT"}],
-  url: [scheme: "https", host: "pacific-shore-04688.herokuapp.com", port: 443],
+  # url: [scheme: "https", host: "pacific-shore-04688.herokuapp.com", port: 443],
+  url: [scheme: "https", host: "pantry.sora.one", port: 443],  
   force_ssl: [rewrite_on: [:x_forwarded_proto]],
   cache_static_manifest: "priv/static/cache_manifest.json"
 
